c62477cc21307e1cd99cb78928e9eec5fe556dae
Anna <annaersh1994@gmail.com>
2022-05-03 23:31:46 +0300

    Initial Commit

---
+++ CopyTextExt05/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  CopyTextExt05
+//
+//  Created by Anna Ershova on 02.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
---
+++ CopyTextExt05/CopyTextExt05App.swift
@@ -0,0 +1,17 @@
+//
+//  CopyTextExt05App.swift
+//  CopyTextExt05
+//
+//  Created by Anna Ershova on 02.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CopyTextExt05App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
